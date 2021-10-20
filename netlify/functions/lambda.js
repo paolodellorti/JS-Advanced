3aaa9b572ad0596a11e5b44db18e301046c6fe06
Paolo <paolodellorti@gmail.com>
2021-10-21 00:46:32 +0100

    small

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -4,16 +4,19 @@ exports.handler = async event => {
   const API_KEY =  process.env.API_KEY
 
   // qui facciamo la chiamata alla API esattamente come la facevamo prima in index_dev.js
-  // const response = await fetch(`https://api.waqi.info/feed/roma/?token=c959a7c7725869fa29e518f16f2ce8bd1c5daafd`)
-  // const data = await response.json() 
+  const response = await fetch(`https://api.waqi.info/feed/roma/?token=${API_KEY}`)
+  const data = await response.json() 
 
   // da qui in giù la funzione fa da back-end: elaboriamo dei dati e li rimandiamo al front-end in formato JSON con uno statusCode 200, cioè "successo".
+  const pass = (body) => {
     return {
       statusCode: 200,
-      body: JSON.stringify(API_KEY)
+      body: JSON.stringify(body)
     }
-}
+  }
 
+  return pass(data)
+}
 
   // const api = {
   //   token: "vivalamamma"
