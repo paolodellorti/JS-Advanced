7d5de538a30571da1361ee9c21774a23b3e51837
Paolo <paolodellorti@gmail.com>
2021-10-21 17:16:05 +0100

    small

--- netlify/functions/lambda.js
+++ netlify/functions/lambda.js
@@ -3,7 +3,7 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 exports.handler = async event => {
 
   const API_KEY =  process.env.API_KEY;
-  const API_PARAMS = event.queryStringParameters;
+  const API_PARAMS = event.queryStringParameters.lat;
 
   const response = await fetch(`https://api.waqi.info/feed/roma/?token=${API_KEY}`);
   const data = await response.json();
